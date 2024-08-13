73b5b9177b29523cf5c4b5e1f65f72ad4aef7dfa
SoliTheUnholy <soheilzss82@gmail.com>
2024-08-13 17:04:34 +0330

    map fin

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -28,10 +28,7 @@ export default function UserLayout({
         <div className="hidden border-r bg-muted/40 md:block">
           <div className="flex h-full max-h-screen flex-col gap-2">
             <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-              <Link href="/" className="flex items-center gap-2 font-semibold">
-                <Package2 className="h-6 w-6" />
-                <span className="">{Today}</span>
-              </Link>
+              <span>{Today}</span>
             </div>
             <div className="flex-1">
               <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
@@ -39,7 +36,6 @@ export default function UserLayout({
                   href="#"
                   className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                 >
-                  <Home className="h-4 w-4" />
                   Add
                 </Link>
               </nav>
@@ -65,14 +61,12 @@ export default function UserLayout({
                     href="#"
                     className="flex items-center gap-2 text-lg font-semibold"
                   >
-                    <Package2 className="h-6 w-6" />
-                    <span className="sr-only"></span>
+                    <span className="sr-only">GIS</span>
                   </Link>
                   <Link
                     href="#"
                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                   >
-                    <Home className="h-5 w-5" />
                     Add
                   </Link>
                 </nav>
